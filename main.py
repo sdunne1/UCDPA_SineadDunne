300d434d50b1589760d199e0f1d8aabe01289f3f
S_Dunne1 <s_dunne1@hotmail.com>
2021-06-03 21:25:01 +0100

    3/6/2021

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # importing libraries
+import pandas
 import pandas as pd
 import numpy as np
 
 # Read in my .csv file
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 drinks = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\drinks.csv')
 
 # First view of my dataset
@@ -16,6 +18,11 @@ drinks = drinks.replace(np.NaN, 0)
 print(drinks.isnull().values.sum())
 
 # Looking for duplicate records
+boolean = drinks.duplicated().any()
+
+# Replacing & symbol in country column to make text easier to work with.
+and_dict = {'&':'and'}
+drinks.replace(and_dict, regex=True, inplace=True)
 
 # Renaming the columns for easiness to work with.
 # Checking the column names updated
@@ -24,3 +31,5 @@ drinks.rename(columns={'beer_servings': 'beer',
                         'spirit_servings': 'spirit',
                         'total_litres_of_pure_alcohol': 'total litres'}, inplace=True)
 print(drinks.columns)
+
+# Adding a new column to dataframe named continent
