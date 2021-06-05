19269d1a0570163415935152aac04e85eca361fc
S_Dunne1 <s_dunne1@hotmail.com>
2021-06-05 22:48:38 +0100

    5/6/2021 2nd

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Read in my .csv file
-pd.set_option("display.max_rows", None, "display.max_columns", None)
+pd.set_option("display.max_rows", 200, "display.max_columns", 5)
 drinks = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\drinks.csv')
 
 # First view of my dataset
@@ -13,24 +13,29 @@ print(drinks.head())
 print(drinks.isnull().values.sum())
 drinks = drinks.replace(np.NaN, 0)
 
-# Checking dataframe to ensure replacing occurred.
+# Checking dataframe to ensure replace was successful.
 print(drinks.isnull().values.sum())
 
 # Looking for duplicate records
 dup = drinks.duplicated().any()
+print(dup)
 
 # Renaming the columns for easiness to work with.
 # Checking the column names updated
 drinks.rename(columns={'beer_servings': 'beer',
-                        'wine_servings': 'wine',
-                        'spirit_servings': 'spirit',
-                        'total_litres_of_pure_alcohol': 'total_litres'}, inplace=True)
-
-# Sorting my dataframe to alphabetical order
-drinks.sort_values("total_litres", ascending=True)
+                       'wine_servings': 'wine', 'spirit_servings': 'spirit',
+                       'total_litres_of_pure_alcohol': 'total_litres'})
+print(drinks)
 
 
-# Adding a new column to dataframe named continent
+# Setting df index column.
+drinks.set_index("country")
+
+# Sorting my dataframe by country with highest total litres alcohol pp/py
+# drinks.sort_values("total_litres", ascending=True)
+
+# Adding a new column to dataframe named continent,using merge and matching on country.
 # Read in 2nd data set .csv file
-pd.set_option("display.max_rows", None, "display.max_columns", None)
 country_con = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\continents.csv')
+
+# Merging datasets drinks and country_con
