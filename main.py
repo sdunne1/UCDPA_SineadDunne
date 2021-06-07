f8f8d29e7d89d8b0b1297403e06f1354dca50438
S_Dunne1 <s_dunne1@hotmail.com>
2021-06-07 21:37:05 +0100

    7/6/2021 2nd

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import numpy as np
 
 # Read in my .csv file, setting index column.
 pd.set_option("display.max_rows", 200, "display.max_columns", 5)
-df_drinks = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\drinks.csv', header = 0, names=['COUNTRY','BEER','SPIRIT','WINE','TOTAL_LITRES'])
+df_drinks = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\drinks.csv', header=0,
+                        names=['COUNTRY', 'BEER', 'SPIRIT', 'WINE', 'TOTAL_LITRES'])
 
 # Setting index for dataframe df_drinks
 df_drinks.set_index('COUNTRY')
@@ -28,7 +29,10 @@ print(dup)
 
 # Adding a new column to dataframe named continent,using merge and matching on country.
 # Read in 2nd data set .csv file
-country_con = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\continents.csv', index_col = ["country"])
+pd.set_option("display.max_rows", 200, "display.max_columns", 5)
+df_countrycon = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\continents.csv', header=0,
+                            names=['COUNTRY', 'CONTINENT'])
+print(df_countrycon)
 
 # Merging datasets drinks and country_con
 # drinks_con = pd.merge()
