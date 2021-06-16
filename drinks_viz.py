7c19ae3b3ed25d154a2e156f74aedf71cc35500a
S_Dunne1 <s_dunne1@hotmail.com>
2021-06-16 23:38:23 +0100

    16/6/2021 2nd

--- drinks_viz.py
+++ drinks_viz.py
@@ -7,26 +7,40 @@ import seaborn as sns
 df_drinks_con = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\df_drinks_con.csv', sep=',', header=0,
                             index_col=0)
 df_drinks_con.sort_values(["TOTAL_LITRES"], ascending=True)
-print(df_drinks_con.head(5))
+print(df_drinks_con.head(4))
 
-# Insight 1: Top 10 countries with the highest total litres consumed per person
+# Visualisation 1: Top 10 countries with the highest total litres consumed per person
 total_by_country = df_drinks_con[['COUNTRY', 'TOTAL_LITRES']].groupby("COUNTRY").sum().sort_values(by='TOTAL_LITRES',
                                                                                           ascending=False).head(10)
 total_by_country.plot(xlabel="COUNTRY", ylabel="TOTAL LITRES",
-                                        kind="bar", title="Total Litres consumed per capita by Country (2010)", rot=45)
+                                        kind="bar", title="Total Litres consumed per capita by Country (2010)", rot=12)
 plt.show()
 
-# Visualisation 1: Scatter plot chart to show sum of servings for each alcohol by continent for year 2010.
-sns.load_dataset(df_drinks_con)
+# Visualisation 2A: Top 10 countries for wine consumers
+wine = df_drinks_con[['COUNTRY', 'WINE']].groupby("COUNTRY").sum().sort_values(by="WINE", ascending=False).head(10)
+ax = sns.barplot(x="WINE", y="COUNTRY", data=wine)
+ax.set(ylabel='COUNTRY', xlabel='WINE SERVINGS')
+ax.set_title("Top 10 Global Wine Consumers")
+
+# Visualisation 2B: Top 10 countries for spirit consumers
+spirits = df_drinks_con[['COUNTRY', 'SPIRIT']].groupby("COUNTRY").sum().sort_values(by="SPIRIT", ascending=False).head(10)
+ax = sns.barplot(x="SPIRIT", y="COUNTRY", data=spirits)
+ax.set(ylabel='COUNTRY', xlabel='SPIRIT SERVINGS')
+ax.set_title("Top 10 Global Spirits Consumers")
+#
+#
+#
+# Scatter plot chart to show sum of servings for each alcohol by continent for year 2010.
+#sns.load_dataset(df_drinks_con)
 
 
 
 # df_drinks_con["TL_SUM"] = df_drinks_con.total_litres.sum()
 
 # scatter_drinks = df_drinks_con.groupby(["CONTINENT"]).agg({'BEER': sum, 'WINE': sum, 'SPIRIT': sum})
-sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="BEER", hue="CONTINENT")
-sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="WINE", hue="CONTINENT")
-sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="SPIRIT", hue="CONTIENT")
-plt.show()
+#sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="BEER", hue="CONTINENT")
+#sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="WINE", hue="CONTINENT")
+#sns.scatterplot(data=df_drinks_con,x='TOTAL_LITRES', y="SPIRIT", hue="CONTIENT")
+#plt.show()
 
 
