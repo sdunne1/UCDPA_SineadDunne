1d7fc08fe011b164aa3f32468f0a6db4c07b1cd3
S_Dunne1 <s_dunne1@hotmail.com>
2021-06-20 22:51:57 +0100

    20/6/2021 1st

--- drinks_viz.py
+++ drinks_viz.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 # Read in merged dataframe csv file: df_drinks_con.csv
 df_drinks_con = pd.read_csv(r'C:\Users\S_Dun\Desktop\UCDPA_SineadDunne\df_drinks_con.csv', sep=',', header=0,
                             index_col=0)
@@ -34,5 +33,57 @@ beer = df_drinks_con.groupby("COUNTRY")["BEER"].max().reset_index().sort_values(
 ax = sns.barplot(x="BEER", y="COUNTRY", data=beer)
 ax.set(ylabel='COUNTRY', xlabel='BEER SERVINGS')
 ax.set_title("Top 10 Global Beer Consumers")
+
+# Visualisation 3
+df_drinks_con['PREF'] = ""
+
+def maxFunction(value1, value2, value3):
+    if (value1 >= value2) and (value1 >= value3):
+        return 1
+    elif (value2 >= value1) and (value2 >= value3):
+        return 2
+    else:
+        return 3
+
+for index, row in df_drinks_con.iterrows():
+    countryName = row.iloc[0]
+    largest = row.index[maxFunction(row.iloc[1],row.iloc[2],row.iloc[3])]
+
+
+    #if(row.iloc[1] >= row.iloc[2]) and (row.iloc[1] >= row.iloc[3]):
+     #   largest = row.index[1]
+    #elif(row.iloc[2] >= row.iloc[1]) and (row.iloc[2] >= row.iloc[3]):
+     #   largest = row.index[2]
+    #else:
+     #   largest = row.index[3]
+    #print(countryName, largest)
+    #new_row = {'COUNTRY': countryName, 'PREF': largest}
+    #print(new_row)
+    df_drinks_con.loc[index,'PREF'] = largest
+
+print(df_drinks_con.head(4))
+
+
+print(df_pref_con)
+# print(df_drinks_con.loc[row['BEER'] & row['WINE'] & row['SPIRIT']].max())
+
+# df_conpref = [df_drinks_con["CONTINENT"]].iloc[:, 1:5]
+# labels = 'Spirit','Beer', 'Total Litres','Wine'
+# colors = ['#2ECC71','#2FF7F7','#F12BBE','#F4D03F']
+# sections = [df_conpref.SPIRIT.sum(),
+#            df_conpref.BEER.sum(),
+#            df_conpref.TOTAL_LITRES.sum() ,
+#            df_conpref.WINE.sum()]
+
+plt.figure(figsize=(14, 8), dpi=75)
+plt.pie(sections, labels=labels,wedgeprops=dict( alpha=1),
+        startangle=90,
+        #explode = (0,0,0,0),
+        autopct = '%0.1f%%',
+         textprops={
+                'fontsize': 10,
+                'fontweight': 'normal'}
+            )
+
 
 
